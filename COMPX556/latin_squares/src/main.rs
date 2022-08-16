4116eded491599d7643366e9649181dc74c6a4db
Anton Lee <anton.j.15.le@gmail.com>
2022-08-16 16:46:53 +1200

    Support partial squares

--- COMPX556/latin_squares/src/main.rs
+++ COMPX556/latin_squares/src/main.rs
@@ -10,21 +10,38 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Commands {
-    /// Generate a new latin square
-    Validate {
-        /// Size of the square
+    /// Print and score a latin square
+    Score {
         #[clap(value_parser)]
         filename: String,
+    },
+    /// Turn a complete latin square into a partial latin square
+    Partial {
+
+        #[clap(value_parser)]
+        proportion: f32,
+        #[clap(value_parser)]
+        in_filename: String,
+        #[clap(value_parser)]
+        out_filename: String,
+
     },
 }
 
 fn main() {
     let cli = Cli::parse();
     match &cli.command {
-        Commands::Validate { filename } => {
+        Commands::Score { filename } => {
             let square = Square::from_json(&filename);
             println!("{}", square);
             println!("Score: {} (0 is a valid latin square)", square.score_square());
+        }
+        Commands::Partial { proportion, in_filename, out_filename } => {
+            let square = Square::from_json(in_filename);
+            let partial_square = square.make_partial(*proportion);
+            println!("{}", partial_square);
+            println!("Score: {} (0 is a valid latin square)", partial_square.score_square());
+            partial_square.to_json(out_filename);
         }
     }
 }
--- COMPX556/latin_squares/src/square.rs
+++ COMPX556/latin_squares/src/square.rs
@@ -1,11 +1,17 @@
 use json;
 use std::fmt;
-use std::io::Read;
+use std::io::{Read, Write};
 
+#[derive(Clone, Copy)]
+pub enum SquareItem {
+    Empty,
+    Treatment(u32),
+}
 
+#[derive(Clone)]
 /// A square of symbols. Can contain latin squares.
 pub struct Square {
-    data: Vec<Vec<u32>>,
+    data: Vec<Vec<SquareItem>>,
     size: usize,
 }
 
@@ -18,7 +24,7 @@ impl Square {
         };
         // Resize the square to the correct size
         square.data.resize(size, Vec::new());
-        square.data.iter_mut().for_each(|v| v.resize(size, 0));
+        square.data.iter_mut().for_each(|v| v.resize(size, SquareItem::Empty));
         return square;
     }
 
@@ -33,40 +39,28 @@ impl Square {
         let mut square = Square::new(json_data.len());
         for i in 0..json_data.len() {
             for j in 0..json_data.len() {
-                square.data[i][j] = json_data[i][j].as_u64().expect("Failed to load latin square") as u32;
+                match &json_data[i][j] {
+                    json::JsonValue::Number(n) => {
+                        let n: f32 = (*n).into();
+                        square.data[i][j] = SquareItem::Treatment(n as u32);
+                    }
+                    json::JsonValue::Short(s) if s == "Empty" => {
+                        square.data[i][j] = SquareItem::Empty;
+                    }
+                    _ => {
+                        panic!("Json contains unexpected data for a latin square");
+                    }
+                }
             }
         }
         return square;
     }
 
-    /// Check a row for duplicates
-    fn check_row(&self, row: usize) -> u32 {
-        let mut duplicates = 0;
-        let mut exists = vec![false; self.size];
-        
-        for col in 0..self.size {
-            let value = self.data[row][col];
-            if exists[value as usize] {
-                duplicates += 1;
-            }
-            exists[value as usize] = true;
-        }
-        return duplicates;
-    }
-    
-    /// Check a column for duplicates
-    fn check_col(&self, col: usize) -> u32 {
-        let mut duplicates = 0;
-        let mut exists = vec![false; self.size];
-        
-        for row in 0..self.size {
-            let value = self.data[row][col];
-            if exists[value as usize] {
-                duplicates += 1;
-            }
-            exists[value as usize] = true;
-        }
-        return duplicates;
+    /// Save square to json file
+    pub fn to_json(&self, filename: &str) {
+        let json_string = json::stringify(self.data.clone());
+        let mut file = std::fs::File::create(filename).expect("Could not create file");
+        file.write_all(json_string.as_bytes()).expect("Could not write to file");
     }
 
     /// Score square ensures that the square is a valid latin square
@@ -80,6 +74,59 @@ impl Square {
         }
         return duplicates;
     }
+
+    /// Create a partial square from a complete square
+    pub fn make_partial(&self, proportion: f32) -> Square {
+        if proportion < 0.0 || proportion > 1.0 {
+            panic!("Proportion must be between 0 and 1");
+        }
+        
+        let mut partial_square = self.clone();
+
+        // Remove items with certain probability
+        for row in 0..self.size {
+            for col in 0..self.size {
+                if rand::random::<f32>() < proportion {
+                    partial_square.data[row][col] = SquareItem::Empty;
+                }
+            }
+        }
+
+        return partial_square;
+    }
+
+    /// Check a row for duplicates
+    fn check_row(&self, row: usize) -> u32 {
+        let mut exists = vec![false; self.size];
+        
+        for col in 0..self.size {
+            let value = self.data[row][col];
+            match value {
+                SquareItem::Empty => {},
+                SquareItem::Treatment(value) => {
+                    exists[value as usize] = true;
+                }
+            }
+        }
+        // Count the number of false values in the vector
+        return exists.iter().filter(|&x| !x).count() as u32;
+    }
+    
+    /// Check a column for duplicates
+    fn check_col(&self, col: usize) -> u32 {
+        let mut exists = vec![false; self.size];
+        for row in 0..self.size {
+            let value = self.data[row][col];
+            match value {
+                SquareItem::Empty => {},
+                SquareItem::Treatment(value) => {
+                    exists[value as usize] = true;
+                }
+            }
+        }
+        // Count the number of false values in the vector
+        return exists.iter().filter(|&x| !x).count() as u32;
+    }
 }
 
 // Pretty print the square
@@ -87,10 +134,70 @@ impl std::fmt::Display for Square {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         for i in 0..self.size {
             for j in 0..self.size {
-                write!(f, "{:02}  ", self.data[i][j])?;
+                match self.data[i][j] {
+                    SquareItem::Empty => write!(f, " . ")?,
+                    SquareItem::Treatment(value) => write!(f, "{:02} ", value)?,
+                }
             }
             write!(f, "\n\n")?;
         }
         return f.write_str("");
+    }
+}
+
+impl From<SquareItem> for json::JsonValue {
+    fn from(item: SquareItem) -> json::JsonValue {
+        match item {
+            SquareItem::Empty => json::JsonValue::String("Empty".to_string()),
+            SquareItem::Treatment(value) => json::JsonValue::Number(value.into()),
+        }
+    }
+}
+
+
+
+
+#[cfg(test)]
+mod tests {
+    use rand::Rng;
+
+    const COMPLETE_SQUARES: [&str;6] = [
+        "data/complete/LatinSquare5.json",
+        "data/complete/LatinSquare8.json",
+        "data/complete/LatinSquare10.json",
+        "data/complete/LatinSquare15.json",
+        "data/complete/LatinSquare20.json",
+        "data/complete/LatinSquare50.json",
+    ];
+
+    #[test]
+    fn score_complete_squares() {
+        for filename in COMPLETE_SQUARES.iter() {
+            let square = super::Square::from_json(filename);
+            assert_eq!(square.score_square(), 0);
+        }
+    }
+
+    #[test]
+    fn score_empty_square() {
+        let square = super::Square::new(5);
+        assert_eq!(square.score_square(), 50);
+    }
+
+    #[test]
+    fn score_bad_square() {
+        let n = 10;
+        let mut rng = rand::thread_rng();
+
+        for filename in COMPLETE_SQUARES.iter() {
+            let mut square = super::Square::from_json(filename);
+            // Randomly swap n elements
+            for _ in 0..n {
+                let i = rng.gen_range(0..square.size);
+                let j = rng.gen_range(0..square.size);
+                square.data[i][j] = super::SquareItem::Treatment(rng.gen_range(0..square.size) as u32);
+            }
+            assert_ne!(square.score_square(), 0);
+        }
     }
 }
